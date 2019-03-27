69192b54468e9f144640d0177ca5f1bfb554c842
amishiratori <amishiratori>
2019-03-27 05:05:54 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models.rb'
+require 'net/http'
+
+get '/' do
+  erb :index
+end
+
+get '/mentor/:id' do
+  @mentor = Mentor.find(params[:id])
+  erb :mentor
+end
+
+post '/peas/:id' do
+  mentor = Mentor.find(params[:id])
+  mentor.update_column(:peas, mentor.peas + 1)
+end
---
+++ db/seeds.rb
@@ -0,0 +1,120 @@
+Mentor.create({
+  name: 'きなこ',
+  img: './images/kinako.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'ちゃんりか',
+  img: './images/chanrika.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'ハラレン',
+  img: './images/hararen.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'みゆせる',
+  img: './images/miyuseru.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'あーみー',
+  img: './images/army.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'みさこ',
+  img: './images/misako.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'ちょむ',
+  img: './images/chomu.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'チャンピオン',
+  img: './images/champion.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'みこ',
+  img: './images/miko.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'モアイ',
+  img: './images/moai.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'うっちー',
+  img: './images/ucchi-.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'わんたか',
+  img: './images/onetaka.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'カーキ',
+  img: './images/khaki.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'ちゃす',
+  img: './images/chasu.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'まさっぺ',
+  img: './images/masappe.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'ちゃーりー',
+  img: './images/charry.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'じんちゃん',
+  img: './images/jinchan.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'わだどん',
+  img: './images/wadadon.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'ちぇる',
+  img: './images/cheru.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'さみー',
+  img: './images/sammy.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'あみたん',
+  img: './images/amitan.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'イカさん',
+  img: './images/ikasan.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'カロリーメイト',
+  img: './images/calorie.jpg',
+  peas: 0
+})
+Mentor.create({
+  name: 'おおってぃー',
+  img: './images/ohta.jpg',
+  peas: 0
+})
